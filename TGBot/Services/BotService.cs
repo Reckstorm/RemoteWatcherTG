120a1cc6335012b7f59c4e4f264807a973a4ce8e
Mihail Sevagin <47269465+Reckstorm@users.noreply.github.com>
2024-07-29 20:56:46 +0300

    Added try catch blocks into the message handlers

--- TGBot/Services/BotService.cs
+++ TGBot/Services/BotService.cs
@@ -25,7 +25,7 @@ namespace TGBot.Services
         {
             _configuration = configuration;
             _mediator = mediator;
-            _botClient = new TelegramBotClient("6616145643:AAGcDgfj23x8wMez9zeiiTXbTEfS9EBM8rk");
+            _botClient = new TelegramBotClient("7131623656:AAHtTW9GZkGFLvGukTM4rst7dXV9m6AKEn4");
             _receiverOptions = new()
             {
                 AllowedUpdates = [UpdateType.Message, UpdateType.CallbackQuery]
@@ -70,36 +70,43 @@ namespace TGBot.Services
             var chatId = update.CallbackQuery.Message.Chat.Id;
             string response = "Invalid command";
 
-            if (callBackData == null)
+            try
             {
-                await botClient.SendTextMessageAsync(chatId, response);
-                return;
+                if (callBackData == null)
+                {
+                    await botClient.SendTextMessageAsync(chatId, response);
+                    return;
+                }
+
+                response = "Choose an action";
+
+                //Base menu actions
+                if (_userRequest.Menu == CommonItems.BackToMain)
+                {
+                    MainMenuHandler.Handle(botClient, update, _userRequest, response, cancellationToken);
+                }
+
+                //Logic menu actions
+                if (_userRequest.Menu == MainMenu.Logic)
+                {
+                    LogicHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
+                }
+
+                //Processes menu actions
+                if (_userRequest.Menu == MainMenu.Process)
+                {
+                    ProcessesHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
+                }
+
+                //Rules menu actions
+                if (_userRequest.Menu == MainMenu.Rule)
+                {
+                    RulesHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
+                }
             }
-
-            response = "Choose an action";
-
-            //Base menu actions
-            if (_userRequest.Menu == CommonItems.BackToMain)
+            catch (Exception ex)
             {
-                MainMenuHandler.Handle(botClient, update, _userRequest, response, cancellationToken);
-            }
-
-            //Logic menu actions
-            if (_userRequest.Menu == MainMenu.Logic)
-            {
-                LogicHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
-            }
-
-            //Processes menu actions
-            if (_userRequest.Menu == MainMenu.Process)
-            {
-                ProcessesHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
-            }
-
-            //Rules menu actions
-            if (_userRequest.Menu == MainMenu.Rule)
-            {
-                RulesHandler.Handle(botClient, update, _userRequest, _mediator, response, cancellationToken);
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -110,61 +117,68 @@ namespace TGBot.Services
             var chatId = update.Message.Chat.Id;
             string response = "Invalid command";
 
-            if (username != _configuration["BotAdminUsername"]) return;
-
-            if (messageText.StartsWith("/menu", true, CultureInfo.CurrentCulture))
+            try
             {
-                response = "Choose an action";
-                _userRequest = new() { Menu = CommonItems.BackToMain };
-                await botClient.SendTextMessageAsync(chatId: chatId, text: response, replyMarkup: InlineKeyboards.MainMenuKeyboard(), parseMode: ParseMode.Html);
-                return;
-            }
+                if (username != _configuration["BotAdminUsername"]) return;
 
-            else if (_userRequest.Menu == MainMenu.Rule && _userRequest.Item == "")
-            {
-                if (CheckName(messageText))
+                if (messageText.StartsWith("/menu", true, CultureInfo.CurrentCulture))
                 {
-                    response = "Select blocker start time";
-                    var name = messageText.Split(' ')[1];
-                    _userRequest.Item = name;
+                    response = "Choose an action";
+                    _userRequest = new() { Menu = CommonItems.BackToMain };
+                    await botClient.SendTextMessageAsync(chatId: chatId, text: response, replyMarkup: InlineKeyboards.MainMenuKeyboard(), parseMode: ParseMode.Html);
+                    return;
+                }
+
+                else if (_userRequest.Menu == MainMenu.Rule && _userRequest.Item == "")
+                {
+                    if (CheckName(messageText))
+                    {
+                        response = "Select blocker start time";
+                        var name = messageText.Split(' ')[1];
+                        _userRequest.Item = name;
+                        await botClient.SendTextMessageAsync(chatId: chatId,
+                            text: response, replyMarkup: await InlineKeyboards.ListKeyboard(CommonItems.BackToRules));
+                        return;
+                    }
                     await botClient.SendTextMessageAsync(chatId: chatId,
-                        text: response, replyMarkup: await InlineKeyboards.ListKeyboard(CommonItems.BackToRules));
+                            text: "Failed to add data.\nTry again according to the provided formats");
                     return;
                 }
-                await botClient.SendTextMessageAsync(chatId: chatId,
-                        text: "Failed to add data.\nTry again according to the provided formats");
-                return;
-            }
 
-            else if (_userRequest.Menu == MainMenu.Rule && _userRequest.Item != "")
-            {
-                if (CheckName(messageText))
+                else if (_userRequest.Menu == MainMenu.Rule && _userRequest.Item != "")
                 {
-                    var name = messageText.Split(' ')[1];
-                    var i = _userRequest.Items.FindIndex(x => x.ProcessName == _userRequest.Item);
-                    var updatedRule = new Domain.Rule { ProcessName = name, BlockEndTime = _userRequest.Items[i].EndTime, BlockStartTime = _userRequest.Items[i].StartTime };
+                    if (CheckName(messageText))
+                    {
+                        var name = messageText.Split(' ')[1];
+                        var i = _userRequest.Items.FindIndex(x => x.ProcessName == _userRequest.Item);
+                        var updatedRule = new Domain.Rule { ProcessName = name, BlockEndTime = _userRequest.Items[i].EndTime, BlockStartTime = _userRequest.Items[i].StartTime };
 
-                    response = "Process name successfully updated";
-                    await KeyboardHandler.HandleFinalRequest(botClient, update, await _mediator.Send(new Application.Rules.Edit.Command { ProcessName = _userRequest.Item, Process = updatedRule }), response, cancellationToken);
-                    _userRequest.Items[i].ProcessName = name;
-                    _userRequest.Item = name;
+                        response = "Process name successfully updated";
+                        await KeyboardHandler.HandleFinalRequest(botClient, update, await _mediator.Send(new Application.Rules.Edit.Command { ProcessName = _userRequest.Item, Process = updatedRule }), response, cancellationToken);
+                        _userRequest.Items[i].ProcessName = name;
+                        _userRequest.Item = name;
 
-                    response = "Choose an action towards the process";
-                    await KeyboardHandler.HandleDetailsRequest(botClient, update, _userRequest, await _mediator.Send(new Application.Rules.Details.Query { ProcessName = name }), InlineKeyboards.RuleMenuKeyboard(), response, cancellationToken);
+                        response = "Choose an action towards the process";
+                        await KeyboardHandler.HandleDetailsRequest(botClient, update, _userRequest, await _mediator.Send(new Application.Rules.Details.Query { ProcessName = name }), InlineKeyboards.RuleMenuKeyboard(), response, cancellationToken);
+                        return;
+                    }
+                    await botClient.SendTextMessageAsync(chatId: chatId,
+                            text: "Failed to update name.\nTry again according to the provided formats");
                     return;
                 }
-                await botClient.SendTextMessageAsync(chatId: chatId,
-                        text: "Failed to update name.\nTry again according to the provided formats");
-                return;
-            }
 
-            else
+                else
+                {
+                    await botClient.SendTextMessageAsync(
+                       chatId: chatId,
+                       text: response,
+                       cancellationToken: cancellationToken);
+                    return;
+                }
+            }
+            catch (Exception ex)
             {
-                await botClient.SendTextMessageAsync(
-                   chatId: chatId,
-                   text: response,
-                   cancellationToken: cancellationToken);
-                return;
+                Console.WriteLine(ex.Message);
             }
         }
 
